6c43ac1831baefdcb8984d8af0852a8f5ee53067
Alexis Fox <alexis.fox@duke.edu>
2025-12-27 02:06:29 -0500

    added checkpoint path

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -14,7 +14,6 @@ echo "RUN_NAME        = $RUN_NAME"
 echo "DATA_PATH       = $DATA_PATH"
 echo "CHECKPOINT_PATH = $CHECKPOINT_PATH"
 
-# --------- Train command ---------
 python pretrain.py \
   arch=trm \
   data_paths="[$DATA_PATH]" \
@@ -22,7 +21,7 @@ python pretrain.py \
   epochs=50000 \
   eval_interval=50000 \
   checkpoint_interval=10000 \
-  checkpoint_path="$CHECKPOINT_PATH" \
+  +checkpoint_path="$CHECKPOINT_PATH" \
   lr=1e-4 puzzle_emb_lr=1e-4 weight_decay=1.0 puzzle_emb_weight_decay=1.0 global_batch_size=128 \
   arch.L_layers=2 \
   arch.H_cycles=3 arch.L_cycles=4 \
