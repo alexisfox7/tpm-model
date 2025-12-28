a8188024c463f592fbdbf23c44795fb8d7e24db6
Alexis Fox <alexis.fox@duke.edu>
2025-12-27 19:55:23 -0500

    small sbatch changes

--- run_pretrain.sh
+++ run_pretrain.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RUN_NAME="maze30x30_trm"
+RUN_NAME="sudoku_extreme_trm"
 DATASET_NAME="sudoku-extreme-1k-aug-1000"
 
 export WANDB_PROJECT="Tiny_progressive_models"
@@ -8,22 +8,28 @@ export WANDB_PROJECT="Tiny_progressive_models"
 DATA_PATH="$WORK_BASE/tpm/data/$DATASET_NAME"
 CHECKPOINT_PATH="$WORK_BASE/tpm/checkpoints/tpm/$RUN_NAME"
 
-mkdir -p "$CHECKPOINT_PATH" 
+mkdir -p "$CHECKPOINT_PATH" logs
 
 echo "RUN_NAME        = $RUN_NAME"
 echo "DATA_PATH       = $DATA_PATH"
 echo "CHECKPOINT_PATH = $CHECKPOINT_PATH"
+
+# test
+LAMBDA_PROG=0.1
 
 python pretrain.py \
   arch=trm \
   data_paths="[$DATA_PATH]" \
   evaluators="[]" \
   epochs=50000 \
-  eval_interval=50000 \
+  eval_interval=10000 \
   checkpoint_interval=10000 \
   +checkpoint_path="$CHECKPOINT_PATH" \
-  lr=1e-4 puzzle_emb_lr=1e-4 weight_decay=1.0 puzzle_emb_weight_decay=1.0 global_batch_size=128 \
+  lr=1e-4 puzzle_emb_lr=1e-4 \
+  weight_decay=1.0 puzzle_emb_weight_decay=1.0 \
+  arch.mlp_t=True arch.pos_encodings=none \
   arch.L_layers=2 \
-  arch.H_cycles=3 arch.L_cycles=4 \
+  arch.H_cycles=3 arch.L_cycles=6 \
+  arch.loss.lambda_prog=$LAMBDA_PROG arch.loss.margin_m=0.0 arch.loss.phi_type=softplus \
   +run_name="$RUN_NAME" \
   ema=True
