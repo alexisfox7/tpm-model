f81df2d23207100e236514a8303a9ea884fcbaab
Alexis Fox <alexis.fox@duke.edu>
2025-12-27 20:36:22 -0500

    add total_loss tracking

--- models/losses.py
+++ models/losses.py
@@ -117,18 +117,25 @@ class ACTLossHead(nn.Module):
             })
             
         q_halt_loss = F.binary_cross_entropy_with_logits(outputs["q_halt_logits"], seq_is_correct.to(outputs["q_halt_logits"].dtype), reduction="sum")
-        metrics.update({
-            "lm_loss": lm_loss.detach(),
-            "q_halt_loss": q_halt_loss.detach(),
-        })
+
         # Q continue (bootstrapping target loss); Alexia: This fits Q-learning, but seems totally unecessary
         q_continue_loss = 0
         if "target_q_continue" in outputs:
             q_continue_loss = F.binary_cross_entropy_with_logits(outputs["q_continue_logits"], outputs["target_q_continue"], reduction="sum")
-
             metrics["q_continue_loss"] = q_continue_loss.detach()
+
+        # Compute total loss
+        total_loss = lm_loss + 0.5 * (q_halt_loss + q_continue_loss) + self.lambda_prog * prog_loss
+
+        # Update metrics with all losses
+        metrics.update({
+            "lm_loss": lm_loss.detach(),
+            "q_halt_loss": q_halt_loss.detach(),
+            "total_loss": total_loss.detach(),
+        })
+
         # Filter outputs for return
         detached_outputs = {k: outputs[k].detach() for k in return_keys if k in outputs}
 
-        return new_carry, lm_loss + 0.5 * (q_halt_loss + q_continue_loss) + self.lambda_prog * prog_loss, metrics, detached_outputs, new_carry.halted.all()
+        return new_carry, total_loss, metrics, detached_outputs, new_carry.halted.all()
 
